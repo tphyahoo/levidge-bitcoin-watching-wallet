15ebfb2c6c0f74461d44e94c6750b197d43c376b
thomas hartman <thomashartman1@gmail.com>
2018-09-23 11:34:07 +0000

    log updates to home directory, outside of source code dir

--- exchangewalletupdater.sh
+++ exchangewalletupdater.sh
@@ -1,3 +1,3 @@
-echo getting transaction $1                                          >> /home/thomas/levidgeWork/exchangeWalletUpdates.txt
-~/installs/bitcoin-0.16.2/bin/bitcoin-cli -testnet gettransaction $1 >> /home/thomas/levidgeWork/exchangeWalletUpdates.txt
+echo getting transaction $1                                          >> /home/thomas/exchangeWalletUpdates.txt
+~/installs/bitcoin-0.16.2/bin/bitcoin-cli -testnet gettransaction $1 >> /home/thomas/exchangeWalletUpdates.txt
 
