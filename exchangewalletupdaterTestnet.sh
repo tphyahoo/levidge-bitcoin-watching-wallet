ee70a82f4191449b1278611c1168bc83586f9566
thomas hartman <thomashartman1@gmail.com>
2018-09-28 19:45:15 +0000

    exchabngeWalletUpdater.sh

--- exchangewalletupdaterTestnet.sh
+++ exchangewalletupdaterTestnet.sh
@@ -1,7 +1,11 @@
+#!/bin/bash -uex
+date                                                                 >> /home/thomas/exchangeWalletUpdatesTestnet.txt
+echo posix time: `date +%s`                                          >> /home/thomas/exchangeWalletUpdatesTestnet.txt
 echo getting transaction $1                                          >> /home/thomas/exchangeWalletUpdatesTestnet.txt
 ~/installs/bitcoin-0.16.3/bin/bitcoin-cli -testnet gettransaction $1 >> /home/thomas/exchangeWalletUpdatesTestnet.txt
-echo decoding transaction $1
-~/installs/bitcoin-0.16.3/bin/bitcoin-cli -testnet decoderawtransaction `~/installs/bitcoin-0.16.2/bin/bitcoin-cli -testnet getrawtransaction %1`
+echo decoding transaction $1                                         >> /home/thomas/exchangeWalletUpdatesTestnet.txt
+~/installs/bitcoin-0.16.3/bin/bitcoin-cli -testnet decoderawtransaction `~/installs/bitcoin-0.16.3/bin/bitcoin-cli -testnet getrawtransaction $1` >> /home/thomas/exchangeWalletUpdatesTestnet.txt
+
 
 
 
