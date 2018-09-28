c63f95733aae78b1a44f07631d38615c0d22038f
thomas hartman <thomashartman1@gmail.com>
2018-09-28 19:25:55 +0000

    update exchange wallet updater to 16.3 and do getrawtransaction

--- exchangewalletupdaterTestnet.sh
+++ exchangewalletupdaterTestnet.sh
@@ -1,3 +1,7 @@
 echo getting transaction $1                                          >> /home/thomas/exchangeWalletUpdatesTestnet.txt
-~/installs/bitcoin-0.16.2/bin/bitcoin-cli -testnet gettransaction $1 >> /home/thomas/exchangeWalletUpdatesTestnet.txt
+~/installs/bitcoin-0.16.3/bin/bitcoin-cli -testnet gettransaction $1 >> /home/thomas/exchangeWalletUpdatesTestnet.txt
+echo decoding transaction $1
+~/installs/bitcoin-0.16.3/bin/bitcoin-cli -testnet decoderawtransaction `~/installs/bitcoin-0.16.2/bin/bitcoin-cli -testnet getrawtransaction %1`
+
+
 
