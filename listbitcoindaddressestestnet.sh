f997042d54d964ac3b10abeaa845e6f7401ae95f
thomas hartman <thomashartman1@gmail.com>
2018-09-29 05:22:58 -0400

    de-thomasify

--- listbitcoindaddressestestnet.sh
+++ listbitcoindaddressestestnet.sh
@@ -1,2 +1,2 @@
 source levidge-bitcoin-watching-wallet.env
-bitcoin-cli -conf=/home/thomas/levidge-bitcoin-watching-wallet/dotfiles/.bitcoin/bitcointestnet.conf -testnet listreceivedbyaddress 0 false true
+bitcoin-cli -conf=`pwd`/dotfiles/.bitcoin/bitcointestnet.conf -testnet listreceivedbyaddress 0 false true
