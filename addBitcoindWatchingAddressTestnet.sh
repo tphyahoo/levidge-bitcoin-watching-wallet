0ae75a5143c6359b3dc4a974ac1c68c5d0549142
thomas hartman <thomashartman1@gmail.com>
2018-10-14 01:35:21 +0000

    de thomasify

--- addBitcoindWatchingAddressTestnet.sh
+++ addBitcoindWatchingAddressTestnet.sh
@@ -1,4 +1,4 @@
-bitcoin-cli -conf=/home/thomas/levidge-bitcoin-watching-wallet/dotfiles/.bitcoin/bitcointestnet.conf -testnet  importmulti "
+bitcoin-cli -conf=`pwd`/dotfiles/.bitcoin/bitcointestnet.conf -testnet  importmulti "
   [
     {
       \"scriptPubKey\" : { \"address\": \"$1\" },
