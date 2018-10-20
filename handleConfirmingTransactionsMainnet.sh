0af93a349a09008f05b06307d3bc4ea9a6adb6fb
thomas hartman <thomashartman1@gmail.com>
2018-10-20 13:19:15 +0000

    support scripts for handleConfirmingTransactions

---
+++ handleConfirmingTransactionsMainnet.sh
@@ -0,0 +1,3 @@
+
+-- see https://levidge.com/api/getInstruments for instrumentId and symbol
+./handleConfirmingTransactions `pwd`/dotfiles/.bitcoin/bitcoinmainnet.conf /home/thomas/watchers/btc/transactions.csv confirmingTransactionsMain.txt "Not sure what Userid" 3 BTC
